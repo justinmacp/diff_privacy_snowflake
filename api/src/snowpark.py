8cff2c4832961c8c7ebef5e5218065b77b8fb1e1
Justin MacPherson <justin.macpherson@ryerson.ca>
2023-11-09 18:15:16 +0100

    Privacy budget is now read from the database

--- api/src/snowpark.py
+++ api/src/snowpark.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, request, abort, make_response, jsonify
-import datetime
 
 # Make the Snowflake connection
 from cryptography.hazmat.backends import default_backend
@@ -7,7 +6,6 @@ from cryptography.hazmat.primitives import serialization
 from snowflake.snowpark import Session
 import snowflake.snowpark.functions as f
 from config import creds
-
 from snowpark_src import laplacian_mechanisms
 
 
@@ -32,15 +30,14 @@ session = connect()
 # Make the API endpoints
 snowpark = Blueprint('snowpark', __name__)
 
-# Top 10 customers in date range
-dateformat = '%Y-%m-%d'
-
-total_budget = 100
+user_privacy_budgets_table_str = "usage_tracking.users.privacy_budgets"
+privacy_budget_col = "privacy_budget"
+api_service_col = "api_service"
+write_mode = "overwrite"
 
 
 @snowpark.route('/dp_sum')
 def dp_sum():
-    global total_budget
     # Validate arguments
     privacy_budget_str = request.args.get('privacy_budget')
     table_name_str = request.args.get('table_name')
@@ -51,9 +48,16 @@ def dp_sum():
         privacy_budget = float(privacy_budget_str)
         lower_bound = int(lower_bound_str)
         upper_bound = int(upper_bound_str)
+        budgets_df = session.table(user_privacy_budgets_table_str)
+        total_budget = (
+            budgets_df
+            .select(privacy_budget_col)
+            .where(f.col(api_service_col) == creds['user'])
+            .collect()[0][0]
+        )
         if privacy_budget <= 0 or total_budget - privacy_budget < 0:
             privacy_budget_error = ValueError(
-                "The privacy budget should be greater 0 and smaller than your overall budget"
+                "The privacy budget should be greater 0 and smaller than your overall budget."
             )
             raise privacy_budget_error
     except:
@@ -61,8 +65,16 @@ def dp_sum():
     try:
         df = session.table(table_name_str)
         res = laplacian_mechanisms.dp_sum(df, f.col(column_name_string), privacy_budget, lower_bound, upper_bound)
-        total_budget = total_budget - privacy_budget
-        print("Your remaining budget is ", total_budget)
+        (
+            budgets_df
+            .withColumn(
+                privacy_budget_col,
+                f.when(f.col(api_service_col) == creds['user'], total_budget - privacy_budget)
+                .otherwise(f.col(api_service_col)))
+            .write
+            .saveAsTable(user_privacy_budgets_table_str, mode=write_mode)
+        )
+        print("Your remaining budget is ", total_budget - privacy_budget)
         return make_response(jsonify(res))
     except:
         abort(500, "Error reading from Snowflake. Check the logs for details.")
@@ -70,72 +82,38 @@ def dp_sum():
 
 @snowpark.route('/dp_count')
 def dp_count():
-    global total_budget
     # Validate arguments
     privacy_budget_str = request.args.get('privacy_budget')
     table_name_str = request.args.get('table_name')
-    privacy_budget = float(privacy_budget_str)
     try:
         privacy_budget = float(privacy_budget_str)
-        
+        budgets_df = session.table(user_privacy_budgets_table_str)
+        total_budget = (
+            budgets_df
+            .where(f.col(api_service_col) == creds['user'])
+            .select(privacy_budget_col)
+        )
+        total_budget = total_budget.collect()[0][0]
         if privacy_budget <= 0 or total_budget - privacy_budget < 0:
             privacy_budget_error = ValueError(
                 "The privacy budget should be greater 0 and smaller than your overall budget"
             )
-            raise privacy_budget_error 
+            raise privacy_budget_error
     except:
         abort(400, "Invalid privacy budget.")
     try:
         df = session.table(table_name_str)
         res = laplacian_mechanisms.dp_count(df, privacy_budget)
-        total_budget = total_budget - privacy_budget
-        print("Your remaining budget is ", total_budget)
+        (
+            budgets_df
+            .withColumn(
+                privacy_budget_col,
+                f.when(f.col(api_service_col) == creds['user'], total_budget - privacy_budget)
+                .otherwise(f.col(api_service_col)))
+            .write
+            .saveAsTable(user_privacy_budgets_table_str, mode=write_mode)
+        )
+        print("Your remaining budget is ", total_budget - privacy_budget)
         return make_response(jsonify(res))
-    except:
-        abort(500, "Error reading from Snowflake. Check the logs for details.")
-
-
-@snowpark.route('/customers/top10')
-def customers_top10():
-    # Validate arguments
-    sdt_str = request.args.get('start_range') or '1995-01-01'
-    edt_str = request.args.get('end_range') or '1995-03-31'
-    try:
-        sdt = datetime.datetime.strptime(sdt_str, dateformat)
-        edt = datetime.datetime.strptime(edt_str, dateformat)
-    except:
-        abort(400, "Invalid start and/or end dates.")
-    try:
-        df = session.table('snowflake_sample_data.tpch_sf10.orders') \
-            .filter((f.col('O_ORDERDATE') >= sdt) & (f.col('O_ORDERDATE') <= edt)) \
-            .group_by(f.col('O_CUSTKEY')) \
-            .agg(f.sum(f.col('O_TOTALPRICE')).alias('SUM_TOTALPRICE')) \
-            .sort(f.col('SUM_TOTALPRICE').desc()) \
-            .limit(10)
-        return make_response(jsonify([x.as_dict() for x in df.to_local_iterator()]))
-    except:
-        abort(500, "Error reading from Snowflake. Check the logs for details.")
-
-
-# Monthly sales for a clerk in a year
-@snowpark.route('/clerk/<clerkid>/yearly_sales/<year>')
-def clerk_monthly_sales(clerkid, year):
-    # Validate arguments
-    try:
-        year_int = int(year)
-    except:
-        abort(400, "Invalid year.")
-    if not clerkid.isdigit():
-        abort(400, "Clerk ID can only contain numbers.")
-    clerkid_str = f"Clerk#{clerkid}"
-    try:
-        df = session.table('snowflake_sample_data.tpch_sf10.orders') \
-            .filter(f.year(f.col('O_ORDERDATE')) == year_int) \
-            .filter(f.col('O_CLERK') == clerkid_str) \
-            .with_column('MONTH', f.month(f.col('O_ORDERDATE'))) \
-            .groupBy(f.col('O_CLERK'), f.col('MONTH')) \
-            .agg(f.sum(f.col('O_TOTALPRICE')).alias('SUM_TOTALPRICE')) \
-            .sort(f.col('O_CLERK'), f.col('MONTH'))
-        return make_response(jsonify([x.as_dict() for x in df.to_local_iterator()]))
     except:
         abort(500, "Error reading from Snowflake. Check the logs for details.")
